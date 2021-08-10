688dc51ad3ae7df4ed6d0c4ee82a0c7a5404df9e
Steve Kang <iam@juneyoung.io>
2021-08-10 20:53:37 +0900

    Add swagger document with custom configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  setupDocument(app);
+
   await app.listen(3000);
+}
+
+async function setupDocument(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Driver License Verification API')
+    .setDescription(
+      'An unofficial driver license verification crawler API service with live status check support.',
+    )
+    .setVersion('0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    customCss: `
+      .topbar { display: none; }
+      .swagger-ui { padding-top: 50px }`,
+    customSiteTitle: 'Driver License Verification API',
+  });
 }
 bootstrap();
