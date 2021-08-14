509569281ccae8a60c0126149f01c3b239272241
Steve Kang <iam@juneyoung.io>
2021-08-14 22:16:35 +0900

    Create interface for requesting data

---
+++ src/verification/application/CreateDriverLicenseVerificationUseCase/api/IRequestData.ts
@@ -0,0 +1,8 @@
+export interface IRequestData {
+  driverName: string;
+  driverBirthdayYear: string;
+  driverBirthdayMonth: string;
+  driverBirthdayDay: string;
+  licenseNumber: string;
+  serialNumber: string;
+}
