ccb591cacca4b5f13c990e5892e864a6769998c4
Steve Kang <iam@juneyoung.io>
2021-08-19 23:11:53 +0900

    Fix wrong http code response at catch clause

--- src/verification/presentation/DriverLicenseVerificationController/DriverLicenseVerificationController.ts
+++ src/verification/presentation/DriverLicenseVerificationController/DriverLicenseVerificationController.ts
@@ -1,5 +1,6 @@
-import { Controller, HttpCode, Post, Body } from '@nestjs/common';
+import { Controller, HttpCode, Post, Body, Res, HttpStatus } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Response } from 'express';
 import { CreateDriverLicenseVerificationUseCase } from '../../application/CreateDriverLicenseVerificationUseCase/CreateDriverLicenseVerificationUseCase';
 import { DriverLicenseVerificationControllerRequestBody } from './DriverLicenseVerificationControllerRequestBody';
 import { DriverLicenseVerificationControllerSuccessfulResponse } from './DriverLicenseVerificationControllerSuccessfulResponse';
@@ -17,20 +18,18 @@ export class DriverLicenseVerificationController {
   @ApiResponse({ status: 400, description: 'Bad Request or Failed', type: DriverLicenseVerificationControllerFailedResponse })
   @ApiResponse({ status: 403, description: 'Forbidden' })
   @ApiResponse({ status: 500, description: 'Internal Server Error' })
-  async useCase(
-    @Body() requestBody: DriverLicenseVerificationControllerRequestBody,
-  ): Promise<DriverLicenseVerificationControllerSuccessfulResponse | DriverLicenseVerificationControllerFailedResponse> {
+  async useCase(@Body() requestBody: DriverLicenseVerificationControllerRequestBody, @Res() response: Response) {
     try {
       const useCase = await this.createDriverLicenseVerificationUseCase.execute({ ...requestBody });
-      return {
+      response.status(HttpStatus.OK).json({
         isSuccess: true,
         verificationResult: useCase.code === 'SUCCESS' ? 'VALID' : 'INVALID',
-      };
+      });
     } catch (error) {
-      return {
+      response.status(HttpStatus.BAD_REQUEST).json({
         isSuccess: false,
         errorMessage: error.message,
-      };
+      });
     }
   }
 }
