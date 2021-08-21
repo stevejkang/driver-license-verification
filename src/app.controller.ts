96054871bc69e704befa51f35e718125807bce80
Steve Kang <iam@juneyoung.io>
2021-08-21 23:47:28 +0900

    Add some swagger decorators to health check route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode } from '@nestjs/common';
+import { ApiOperation, ApiProperty, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+interface IHealthCheckResult {
+  ok: boolean;
+}
+
+class HealthCheckResponse {
+  @ApiProperty({
+    description: 'Health Check Result',
+  })
+  ok: boolean;
+}
+
+@ApiTags('Health Check')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @HttpCode(200)
+  @ApiOperation({ summary: 'Health check' })
+  @ApiResponse({ status: 200, description: 'Response Successfully', type: HealthCheckResponse })
   getHealthCheck(): IHealthCheckResult {
     return {
       ok: this.appService.getOk(),
     };
   }
-}
-
-interface IHealthCheckResult {
-  ok: boolean;
 }
