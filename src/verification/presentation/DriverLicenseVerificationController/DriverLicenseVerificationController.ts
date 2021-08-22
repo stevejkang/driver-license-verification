ed49772d8c739b42136fa8397ff45829126ff58d
Steve Kang <iam@juneyoung.io>
2021-08-22 18:47:46 +0900

    Set response http code to 500 when internal error

--- src/verification/presentation/DriverLicenseVerificationController/DriverLicenseVerificationController.ts
+++ src/verification/presentation/DriverLicenseVerificationController/DriverLicenseVerificationController.ts
@@ -16,8 +16,7 @@ export class DriverLicenseVerificationController {
   @ApiOperation({ summary: 'Verify License Information' })
   @ApiResponse({ status: 200, description: 'Response Successfully', type: DriverLicenseVerificationControllerSuccessfulResponse })
   @ApiResponse({ status: 400, description: 'Bad Request or Failed', type: DriverLicenseVerificationControllerFailedResponse })
-  @ApiResponse({ status: 403, description: 'Forbidden' })
-  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error', type: DriverLicenseVerificationControllerFailedResponse })
   async useCase(@Body() requestBody: DriverLicenseVerificationControllerRequestBody, @Res() response: Response) {
     try {
       const useCase = await this.createDriverLicenseVerificationUseCase.execute({ ...requestBody });
@@ -27,7 +26,8 @@ export class DriverLicenseVerificationController {
       });
     } catch (error) {
       const errorMessage = error.name === 'InternalApiRequestError' ? 'Internal server error occured.' : error.message;
-      response.status(HttpStatus.BAD_REQUEST).json({
+      const responseHttpCode = error.name === 'InternalApiRequestError' ? HttpStatus.INTERNAL_SERVER_ERROR : HttpStatus.BAD_REQUEST;
+      response.status(responseHttpCode).json({
         isSuccess: false,
         errorCode: 0,
         errorMessage: errorMessage,
