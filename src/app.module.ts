e29e5928e8430a80f2b510c9b5ae2e95487843c0
Steve Kang <iam@juneyoung.io>
2021-08-24 22:23:53 +0900

    Add @nestjs/config module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VerificationModule } from './verification/VerificationModule';
 
 @Module({
-  imports: [VerificationModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    VerificationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
