cab1fc79a2e42c92c4fa34d999f688d4ffa750b8
Steve Kang <iam@juneyoung.io>
2022-02-02 15:40:32 +0900

    Add env to travis and refactor api unit test code

--- src/verification/application/CreateDriverLicenseVerificationUseCase/api/SafeDriving.spec.ts
+++ src/verification/application/CreateDriverLicenseVerificationUseCase/api/SafeDriving.spec.ts
@@ -1,7 +1,5 @@
-import { INestApplication } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppModule } from '../../../../app.module';
 import { DriverBirthday } from '../../../domain/DriverBirthday';
 import { DriverName } from '../../../domain/DriverName';
 import { LicenseNumber } from '../../../domain/LicenseNumber';
@@ -10,37 +8,37 @@ import { DriverLicense } from '../../../domain/DriverLicense';
 import { SafeDriving } from './SafeDriving';
 
 describe('SafeDriving', () => {
-  let app: INestApplication;
+  let app: ConfigService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: '.env',
+        }),
+      ],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+    app = moduleFixture.get<ConfigService>(ConfigService);
   });
 
   it('should success', async () => {
-    const configService = app.get(ConfigService);
-
     const driverLicense = DriverLicense.createNew({
-      driverName: DriverName.create(configService.get('TEST_DRIVER_NAME')).value,
-      driverBirthday: DriverBirthday.create(configService.get('TEST_DRIVER_BIRTHDAY')).value,
-      licenseNumber: LicenseNumber.create(configService.get('TEST_LICENSE_NUMBER')).value,
-      serialNumber: SerialNumber.create(configService.get('TEST_SERIAL_NUMBER')).value,
+      driverName: DriverName.create(app.get('TEST_DRIVER_NAME')).value,
+      driverBirthday: DriverBirthday.create(app.get('TEST_DRIVER_BIRTHDAY')).value,
+      licenseNumber: LicenseNumber.create(app.get('TEST_LICENSE_NUMBER')).value,
+      serialNumber: SerialNumber.create(app.get('TEST_SERIAL_NUMBER')).value,
     });
 
     expect((await SafeDriving.retrieve(driverLicense.value)).verified).toEqual(true);
   });
 
   it('should success without serial number', async () => {
-    const configService = app.get(ConfigService);
-
     const driverLicenseWithOutSerialNumber = DriverLicense.createNew({
-      driverName: DriverName.create(configService.get('TEST_DRIVER_NAME')).value,
-      driverBirthday: DriverBirthday.create(configService.get('TEST_DRIVER_BIRTHDAY')).value,
-      licenseNumber: LicenseNumber.create(configService.get('TEST_LICENSE_NUMBER')).value,
+      driverName: DriverName.create(app.get('TEST_DRIVER_NAME')).value,
+      driverBirthday: DriverBirthday.create(app.get('TEST_DRIVER_BIRTHDAY')).value,
+      licenseNumber: LicenseNumber.create(app.get('TEST_LICENSE_NUMBER')).value,
       serialNumber: SerialNumber.create(null).value,
     });
 
@@ -48,12 +46,10 @@ describe('SafeDriving', () => {
   });
 
   it('should fail when wrong information', async () => {
-    const configService = app.get(ConfigService);
-
     const driverLicenseWithWrongInformation = DriverLicense.createNew({
-      driverName: DriverName.create(configService.get('TEST_DRIVER_NAME')).value,
-      driverBirthday: DriverBirthday.create(configService.get('TEST_DRIVER_BIRTHDAY')).value,
-      licenseNumber: LicenseNumber.create(configService.get('TEST_LICENSE_NUMBER')).value,
+      driverName: DriverName.create(app.get('TEST_DRIVER_NAME')).value,
+      driverBirthday: DriverBirthday.create(app.get('TEST_DRIVER_BIRTHDAY')).value,
+      licenseNumber: LicenseNumber.create(app.get('TEST_LICENSE_NUMBER')).value,
       serialNumber: SerialNumber.create('123456').value,
     });
 
